488f061bbf119a7dd336b9ef347d8e15a8fa346d
ExonGR <54857227+ExonGR@users.noreply.github.com>
2019-09-05 00:48:10 +0300

    Add files via upload

---
+++ commands/ping.js
@@ -0,0 +1,19 @@
+
+const discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+
+	name: 'ping',
+	
+	
+		message.channel.send('Pong.');
+}
+
+
+
+module.exports.help = {
+  name: "ping"
+};
+
+
+
